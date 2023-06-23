e1c8d3452c053a628cb2dfbccad33477eb96304c
jamieheng <hengomega678@gmail.com>
2023-06-23 23:58:20 +0700

    sfs

--- mainpage/index.php
+++ mainpage/index.php
@@ -513,34 +513,7 @@ if (isset($_GET["logout"])) {
     </div>
 
 
-    <script src="./assets/js/jquery.min.js"></script>
-    <script src="./assets/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/js/owl.carousel.min.js"></script>
-    <script src="./assets/js/waypoints.min.js"></script>
-    <script src="./assets/js/jquery.counterup.min.js"></script>
-    <script src="./assets/js/TweenMax.min.js"></script>
-    <script src="./assets/js/wow.js"></script>
-    <script src="./assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
-    <script src="./assets/js/swiper.min.js"></script>
-    <script src="./assets/js/typed-2.0.11.js"></script>
-    <script src="./assets/js/vegas.min.js"></script>
-    <script src="./assets/js/jquery.validate.min.js"></script>
-    <script src="./assets/js/bootstrap-select.min.js"></script>
-    <script src="./assets/js/countdown.min.js"></script>
-    <script src="./assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="./assets/js/bootstrap-datepicker.min.js"></script>
-    <script src="./assets/js/nouislider.min.js"></script>
-    <script src="./assets/js/isotope.js"></script>
-    <script src="./assets/js/appear.js"></script>
-    <script src="./assets/js/jarallax.js"></script>
-    <script src="./assets/js/jquery.bootstrap-touchspin.js"></script>
-    <script src="./assets/js/jquery-ui.js"></script>
-    <script src="http://maps.google.com/maps/api/js?key=AIzaSyATY4Rxc8jNvDpsK8ZetC7JyN4PFVYGCGM"></script>
 
-
-    <!-- template scripts -->
-    <script src="./assets/js/theme.js"></script>
 
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
